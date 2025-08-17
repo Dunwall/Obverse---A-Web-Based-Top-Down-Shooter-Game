6637a12d0ff4f623949daa3e3138d29d402b95f8
Jordan Jovanny <pdead574@gmail.com>
2025-08-17 17:42:41 +0530

    player initialization
    add basic player sprite and movement

---
+++ game/player.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+@export var movespeed: float = 500.0
+
+func _physics_process(delta: float) -> void:
+	var motion = Vector2.ZERO
+	
+	if Input.is_action_pressed("up"):
+		motion.y -= 1
+	if Input.is_action_pressed("down"):
+		motion.y += 1
+	if Input.is_action_pressed("left"):
+		motion.x -= 1
+	if Input.is_action_pressed("right"):
+		motion.x += 1
+	motion = motion.normalized()
+	velocity = motion * movespeed
+	move_and_slide()
