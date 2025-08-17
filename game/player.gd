8829f29db2c3d9005fe4bb6596291409a5934c9e
Jordan Jovanny <pdead574@gmail.com>
2025-08-17 17:49:05 +0530

    Update player.gd
    Add mouse mechanic, resolve opposing movement key issue

--- game/player.gd
+++ game/player.gd
@@ -1,18 +1,22 @@
 extends CharacterBody2D
 
-@export var movespeed: float = 500.0
+@export var movespeed: float = 600.0
 
 func _physics_process(delta: float) -> void:
 	var motion = Vector2.ZERO
-	
+
 	if Input.is_action_pressed("up"):
 		motion.y -= 1
-	if Input.is_action_pressed("down"):
+	elif Input.is_action_pressed("down"):
 		motion.y += 1
 	if Input.is_action_pressed("left"):
 		motion.x -= 1
-	if Input.is_action_pressed("right"):
+	elif Input.is_action_pressed("right"):
 		motion.x += 1
+
 	motion = motion.normalized()
+
 	velocity = motion * movespeed
 	move_and_slide()
+
+	look_at(get_global_mouse_position())
